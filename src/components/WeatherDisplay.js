26c67e1de59009b27f1a8c4fc832c0ee67d49dbb
Haris <mohammadharis7871@gmail.com>
2024-11-06 16:50:21 +0530

    weatherdisply

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+const WeatherDisplay = ({ weather }) => {
+    const { temperature, conditions } = weather;
+    return (
+        <div>
+            <p>Temperature: <span style={{ color: temperature > 20 ? "red" : "blue" }}>{temperature}</span></p>
+            <p>Conditions: {conditions}</p>
+        </div>
+    )
+}
+
+export default WeatherDisplay;
